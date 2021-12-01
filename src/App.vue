d279e397f5565a4b4f3c208d4cb28af6dfac7d9e
X1361133705 <1361133705@qq.com>
2021-12-02 01:57:50 +0800

    newDome

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h2>-----app内容------</h2>
+    <h2>{{$store.state.counter}}</h2>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+    
+    <h2>-----hellovuex内容------</h2>
+    <hellovuex />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import hellovuex from './components/hellovuejs.vue'
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  export default {
+    name: 'app',
+    components:{
+      hellovuex
+    },
+    data() {
+      return {
+        message: '我是APP组件'
+      }
+    }
+  }
+</script>
---
+++ src/components/hellovuejs.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>{{$store.state.counter}}</h2>
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'hellovuejs'
+  }
+</script>
+
+<style>
+</style>
